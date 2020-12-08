7b3d39a3742abb0e5ffe5aea886241c6f2d579d8
bela <38119769+belaburrito@users.noreply.github.com>
2020-12-08 01:00:17 -0500

    update splash and add random colors

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-function ninjaVanish() {
-    var x = document.getElementById("menu");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//random background color
+const background = document.querySelector('body');
+let randomHue = Math.floor(Math.random()*360);
+let randomHue2 = Math.floor(Math.random()*360);
+console.log(randomHue);
+background.style.background = 'hsl(' + randomHue + ', 100%, 50%)';
+background.style.color = 'hsl(' + randomHue2 + ', 100%, 50%)';
