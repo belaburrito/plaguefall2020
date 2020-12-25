4ec1ea33a7f2eb119338ac2b715bf00916f32889
bela <38119769+belaburrito@users.noreply.github.com>
2020-12-25 00:38:28 -0500

    menu fix, add faq

--- lost/modal.js
+++ lost/modal.js
@@ -51,8 +51,18 @@ function modal() {
     document.getElementsByClassName("modaltxt")[0].innerHTML = "<p>Life create human. Human create trash. Out of trash, come rat. Many rat. Baby rats all swaddled in stinky trash like twinkle twinkle star. Then baby rats grows up. Statistically one rat is chef a little bit. Then chef a lot so good. Rat become Rat Boyardee like American prince of tin can ravioluss, only not American. Is French now. Or French always I think. Movie don’t show rat take American Airlines to Baguette City or anything. Maybe rat born in France. It is possible. But rat speak like American difficult to work with Patton Oswalt so I confuse. Anyway we still proud of rat even if he’s not American citizen.I feed Boyardee ravioluss to my dog he likes so much. He taste rat chunks in Boyardee ravioluss. I think rat chunks is made of Remy rat family. The cousins that no believe Remy cooking amazing like God. How you like him now cousins. Now that you are dead and also in chunks. Ha ha.</p>";
   }
 
-  //change modal content
-  console.log(trigger.length);
+  trigger[5].onclick = function() {
+      modal.classList.add('modal--show');
+      document.getElementsByClassName("modalhead")[0].innerHTML = "<h2> Teeth vs Toothbrush </h2>";
+      document.getElementsByClassName("modaltxt")[0].innerHTML = "<p><i>I. Toothbrush</i></p><p>Like any individual with decent oral hygiene, I change my toothbrush once every three months. Little did I know, however, that when I toss one out, it does not poof-gone like the commercials say. Instead, it winds up somewhere called a “Staten Island,” where it rots among the other herpes-ridden toothbrushes of the world and of Staten Island, which I have been told is not really part of the world.</p><p>I find this deeply disturbing—the toothbrush disposal part, not the bit about political geography. My heart goes out to the citizens of Staten Island—everyone knows that a coastal economy needs its fishing industry to survive, but it is also a fact that no one eats toothbrushes.</p><p>Just picture it: a leathered Staten Island seafarer rowing to shore after a long day on the briny expanse. The fisherman ties up his dinghy and trudges home, his chum bucket cling-clanging with 46 Oral B’s and a barnacle-encrusted tampon. He is distraught—nay, sad. Mud for dinner again tonight.</p><p>And just think of the turtles! What would happen if a 350-pound Loggerhead mistook “the toothbrush that 9 out of 10 dentists recommend” for a skinny row of corn? Corn, lest we forget, is a staple of their wild diet. If turtles start swallowing toothbrushes, they’ll have necks like giraffes, and you know, that’s not kosher. A turtle-giraffe? It doesn’t even sound cool, like a zeedonk (also known as: a zebronkey, a zonkey, a zebadonk, or a deebra—not you Debra. Sit, sit, sit, hey, siiiit. Debra. Debra, sit. Good girl.). It’s going off the rails! The global toothbrush crisis, I mean.</p><p><i>II. Toothpaste</i><p>It’s Thursday. I have run out of my favorite charcoal toothpaste—the one that isn’t a paste but really more of a powder? Anyway, I knew exactly where to go to purchase a refill: an Instagram ad from my favorite doula, Snoop Dogg.</p><p>I was in the online checkout, ready to put my $6 tooth dust on layaway, when I learned that this dehydrated black tar heroin is also infused with mica. Yes, mica—the mineral mined by little brown babies in caves all over rural India. So many caves, at least four caves.</p><p>TBH, still bought it. In this economy, I think elementary-school kids can afford to make some extra money on the side. I mean, they’re not really elementary-school kids; they don’t go to school. They work in mines—I think I’ve mentioned this. So I guess it’s not so much a side gig as a front gig. Well, Lean In, as they say. Lean into the mines.</p>";
+    }
+
+    trigger[6].onclick = function() {
+        modal.classList.add('modal--show');
+        document.getElementsByClassName("modalhead")[0].innerHTML = "<h2> FAQ </h2>";
+        document.getElementsByClassName("modaltxt")[0].innerHTML = "<p>Text here.</p>";
+      }
+
 
 
 
--- lost/script.js
+++ lost/script.js
@@ -1,49 +1,61 @@
-let link1 = document.getElementById("link1");
-let link2 = document.getElementById("link2");
-let link3 = document.getElementById("link3");
-let link4 = document.getElementById("link4");
-let link5 = document.getElementById("link5");
-
-link1.addEventListener("mouseover", function( event ) {
+link0.addEventListener("mouseover", function( event ) {
   event.target.src="imoontravel.png";
 }, false);
 
-link1.addEventListener("mouseout", function( event ) {
+link0.addEventListener("mouseout", function( event ) {
   event.target.src="moontravel.png";
 }, false);
 
 
-link2.addEventListener("mouseover", function( event ) {
+link1.addEventListener("mouseover", function( event ) {
   event.target.src="ibombsoveruterus.png";
 }, false);
 
-link2.addEventListener("mouseout", function( event ) {
+link1.addEventListener("mouseout", function( event ) {
   event.target.src="bombsoveruterus.png";
 }, false);
 
 
-link3.addEventListener("mouseover", function( event ) {
+link2.addEventListener("mouseover", function( event ) {
   event.target.src="imysmoothiedaughter.png";
 }, false);
 
-link3.addEventListener("mouseout", function( event ) {
+link2.addEventListener("mouseout", function( event ) {
   event.target.src="mysmoothiedaughter.png";
 }, false);
 
 
-link4.addEventListener("mouseover", function( event ) {
+link3.addEventListener("mouseover", function( event ) {
   event.target.src="inoteto.png";
 }, false);
 
-link4.addEventListener("mouseout", function( event ) {
+link3.addEventListener("mouseout", function( event ) {
   event.target.src="noteto.png";
 }, false);
 
 
-link5.addEventListener("mouseover", function( event ) {
+link4.addEventListener("mouseover", function( event ) {
   event.target.src="iratatouille.png";
 }, false);
 
-link5.addEventListener("mouseout", function( event ) {
+link4.addEventListener("mouseout", function( event ) {
   event.target.src="ratatouille.png";
+}, false);
+
+
+link5.addEventListener("mouseover", function( event ) {
+  event.target.src="itvt.png";
+}, false);
+
+link5.addEventListener("mouseout", function( event ) {
+  event.target.src="tvt.png";
+}, false);
+
+
+link6.addEventListener("mouseover", function( event ) {
+  event.target.src="ifaq.png";
+}, false);
+
+link6.addEventListener("mouseout", function( event ) {
+  event.target.src="faq.png";
 }, false);
