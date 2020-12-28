ab6da292b1c6b83049125b0b364ed259fb57906b
bela <38119769+belaburrito@users.noreply.github.com>
2020-12-28 10:12:33 -0500

    random camilla vid

--- script.js
+++ script.js
@@ -2,6 +2,31 @@
 const background = document.querySelector('body');
 let randomHue = Math.floor(Math.random()*360);
 let randomHue2 = Math.floor(Math.random()*360);
-console.log(randomHue);
 background.style.background = 'hsl(' + randomHue + ', 100%, 50%)';
 background.style.color = 'hsl(' + randomHue2 + ', 100%, 50%)';
+
+//random video
+let randomVid = Math.floor(Math.random()*3+1);
+var video = document.getElementById('video');
+var source = document.createElement('source');
+
+if(randomVid==1){
+  source.setAttribute('src', 'birds.mov');
+
+  video.appendChild(source);
+  video.play();
+}
+
+if(randomVid==2){
+  source.setAttribute('src', 'kafka.mov');
+
+  video.appendChild(source);
+  video.play();
+}
+
+if(randomVid==3){
+  source.setAttribute('src', 'eye.mov');
+
+  video.appendChild(source);
+  video.play();
+}
